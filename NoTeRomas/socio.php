9ced4e19d6813465abf0de0aa26aec6456c26700
Angelo <anjodanan@hotmail.com>
2018-01-09 09:04:04 +0100

    Actualizado 9-01-18

--- NoTeRomas/socio.php
+++ NoTeRomas/socio.php
@@ -7,9 +7,58 @@
         <style>a{margin: 0 10px}</style>
     </head>
     <body>
-         <h1>Página del Administrador</h1>
+        
         <?php
-        // put your code here
+        session_start();
+        $usuario=$_SESSION["user"];
         ?>
+        <h1>Página del Socio</h1>
+        <h2>Bienvenido <?php echo ucfirst($usuario); ?></h2>
+        <h3>Actividades disponibles</h3>
+        <table style="width:100%; text-align: center;">
+            <tr><th>Nombre</th><th>Precio</th><th>Capacidad</th></tr>
+        <?php
+        require_once 'bbdd.php';
+        $posicion = 0;
+
+        if (isset($_GET["posicion"])) {
+            $posicion = $_GET["posicion"];
+        } else {
+            $posicion = 0;
+        }
+        $actividades = actividades($posicion);
+        $total = totalActividades();
+            while($fila = mysqli_fetch_array($actividades)){
+                extract($fila);
+                echo"<tr><td>$name</td><td>$price €/mes</td><td>$capacity</td><td>";
+            }
+
+        ?>
+             </table>
+             <br>
+            <div>
+                <?php
+                
+                        if($posicion>0){  
+            echo "<a href='socio.php?posicion=".($posicion - 5)."'>prev</a>";
+        }
+        
+        if($posicion + 5 <=$total){
+            echo "Mostrando " . ($posicion + 1) . " al " . ($posicion + 5) . " de $total ";
+        }
+        else {
+            echo "Mostrando " . ($posicion + 1) . " al $total de $total";
+        }
+        
+        if ($posicion + 5 < $total) {
+            echo "<a href='socio.php?posicion=" .($posicion + 5)."'>next</a>";
+        }
+                
+                ?>    
+            </div>
+             <br>
+           
+         <br>
+         <h4>Coste mensual del total de las actividades:</h4>
     </body>
 </html>
