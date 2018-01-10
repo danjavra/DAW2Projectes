41c2d135d61e525542ba4081e2f38e318cc297f7
Angelo <anjodanan@hotmail.com>
2018-01-10 10:35:50 +0100

    Actualizado 10-01-18

--- practica06_M06/practica06.js
+++ practica06_M06/practica06.js
@@ -4,6 +4,7 @@ var valorWindowHeight = window.innerHeight;
 var valorWindowWidth = window.innerWidth;
 var valorWebHeight = screen.height;
 var valorWebWidth = screen.width;
+var urlWeb = location.href;
 
 var divLista = document.getElementById("listaPropiedades");
 divLista.innerHTML = "<ul>"+
@@ -13,6 +14,7 @@ divLista.innerHTML = "<ul>"+
         "<li>"+"Anchura Pantalla: "+valorWindowWidth+"</li>"+
         "<li>"+"Altura Web: "+valorWebHeight+"</li>"+
         "<li>"+"Anchura Web: "+valorWebWidth+"</li>"+
+        "<li>"+"URL de la Web: "+urlWeb+"</li>"+
 
         "</ul>";
 
