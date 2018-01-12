49a5e8d69ec56805f9034efaaffd0adfde02487e
Angelo <anjodanan@hotmail.com>
2018-01-12 13:05:19 +0100

    Actualizado 12-01-2018

---
+++ maquetacion_bootstrap/maquetacion1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="svg-with-js/js/fontawesome-all.js" type="text/javascript"></script>
+  <style>
+         .row1>div{ height: 70px;}
+         
+        </style>
+</head>
+<body>
+    <div class="row1">
+        <div style="background-color: #545454; padding-bottom: 30px; padding-top: 30px; " class="col">
+            <div class="container">
+                <i class="fas fa-phone" style="color: #ffffff; transform: rotate(100deg)"></i><span style="color: white;">+62 054 895 664</span>
+            
+            </div>
+        </div>
+    </div>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">ETERNA</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Features</a></li>
+      <li><a href="#">Pages</a></li>
+      <li><a href="#">Portofolio</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h3>Basic Navbar Example</h3>
+  <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+</div>
+
+</body>
+</html>
--- practica06_M06/practica06.js
+++ practica06_M06/practica06.js
@@ -1,10 +1,14 @@
 var valorMaxim = Number.MAX_VALUE;
 var valorMinim = Number.MIN_VALUE;
-var valorWindowHeight = window.innerHeight;
-var valorWindowWidth = window.innerWidth;
-var valorWebHeight = screen.height;
-var valorWebWidth = screen.width;
+var valorWindowHeight = screen.availHeight;
+var valorWindowWidth = screen.availWidth;
+var valorWebHeight = window.innerHeight;
+var valorWebWidth = window.innerWidth;
 var urlWeb = location.href;
+var indexBarra = urlWeb.lastIndexOf("/");
+var nombre = urlWeb.substr(indexBarra+1);
+var random = Math.floor(Math.random() * 201);
+var os = navigator.appVersion;
 
 var divLista = document.getElementById("listaPropiedades");
 divLista.innerHTML = "<ul>"+
@@ -15,6 +19,8 @@ divLista.innerHTML = "<ul>"+
         "<li>"+"Altura Web: "+valorWebHeight+"</li>"+
         "<li>"+"Anchura Web: "+valorWebWidth+"</li>"+
         "<li>"+"URL de la Web: "+urlWeb+"</li>"+
-
+        "<li>"+"Nombre de la Web con su extensión (index.html): "+nombre+"</li>"+
+        "<li>"+"Un valor aleatorio entre 0 y 200: "+random+"</li>"+
+        "<li>"+"Sistema operativo del ordenador: "+os+"</li>"+
         "</ul>";
 
