75aa5534600da3dc5ab1f5a0e45c7f653f9c0c1a
Anjo <Anjo@DESKTOP-1T8BM4B>
2018-01-29 21:46:04 +0100

    Actualizada hasta la pregunta 4

--- practica07_M06_Sintesis/practica07.js
+++ practica07_M06_Sintesis/practica07.js
@@ -7,8 +7,7 @@ var profunditatColor = screen.colorDepth;
 var profunditatPixel = screen.pixelDepth;
 
 var urlWeb = location.href;
-var indexBarra = urlWeb.lastIndexOf("/");
-var nombre = urlWeb.substr(indexBarra+1);
+var port = location.port;
 
 var os = navigator.appVersion;
 var browser = navigator.appName;
@@ -40,5 +39,27 @@ divLista2.innerHTML = "<ul>"+
 var divLista3 = document.getElementById("listaPropiedadesHostweb");
 divLista3.innerHTML = "<ul>"+
         "<li>"+"URL de la Web: "+urlWeb+"</li>"+
+        "<li>"+"Port de la Web: "+port+"</li>"+
+        "<br><a href='https://www.stucom.com/' class='btn btn-warning btn-sm'>Link web de Stucom</a>"+
       
         "</ul>";
+
+var divLista4 = document.getElementById("glyphIcon1");
+divLista4.innerHTML = 
+        "<a style='text-align: center; font-size: 40px'><span class='glyphicon glyphicon-camera'></span></a>";
+
+var divLista5 = document.getElementById("glyphIcon2");
+divLista5.innerHTML = 
+        "<a style='text-align: center; font-size: 40px'><span class='glyphicon glyphicon-globe'></span></a>";
+
+var divLista6 = document.getElementById("glyphIcon3");
+divLista6.innerHTML = 
+        "<a style='text-align: center; font-size: 40px'><span class='glyphicon glyphicon-film'></span></a>";
+
+var divLista7 = document.getElementById("glyphIcon4");
+divLista7.innerHTML = 
+        "<a style='text-align: center; font-size: 40px'><span class='glyphicon glyphicon-headphones'></span></a>";
+
+var divLista8 = document.getElementById("glyphIcon5");
+divLista8.innerHTML = 
+        "<a style='text-align: center; font-size: 40px'><span class='glyphicon glyphicon-gift'></span></a>";
